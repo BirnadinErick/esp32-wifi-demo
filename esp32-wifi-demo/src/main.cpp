f3762250ee3e67921ae4acd2bfa5f15396d83da3
BirnadinErick <45619033+BirnadinErick@users.noreply.github.com>
2025-04-26 13:05:57 +0200

    add WiFi Manager

--- esp32-wifi-demo/src/main.cpp
+++ esp32-wifi-demo/src/main.cpp
@@ -1,58 +1,33 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <HTTPClient.h>
+#include <WiFiManager.h>
 
-const char *SERVER = "http://";
-
-void initWiFi()
-{
-  const char *WIFI_SSID = "";
-  const char *WIFI_PASSWORD = "";
-
-  WiFi.mode(WIFI_OFF);
-  delay(1000);
-  WiFi.mode(WIFI_STA);
-
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting to WiFi.");
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    switch (WiFi.status())
-    {
-    case WL_CONNECT_FAILED:
-      Serial.println("connection failed");
-      break;
-    
-    case WL_NO_SSID_AVAIL:
-      Serial.println("ssid search failed");
-      break;
-    case WL_IDLE_STATUS:
-      Serial.println("idle status");
-      break;
-    case WL_SCAN_COMPLETED:
-      Serial.println("scan complete, connecting...");
-      break;
-    default:
-      break;
-    }
-  }
-
-  Serial.print("\nIP: ");
-  Serial.print(WiFi.localIP());
-  Serial.print('\n');
-}
+const char *INIT_AP_NAME = "";
 
 void setup()
 {
   Serial.begin(115200);
-  initWiFi();
+  WiFi.mode(WIFI_STA);
+  delay(1000);
+
+  WiFiManager wm;
+  bool res;
+
+  Serial.print("connecting to WiFi...");
+  wm.resetSettings();
+  res = wm.autoConnect(INIT_AP_NAME);
+
+  if (res)
+  {
+    Serial.println("done.");
+  }
+  else
+  {
+    Serial.println("failed! restarting...");
+    ESP.restart();
+  }
 }
 
 void loop()
 {
-  if(WiFi.status() != WL_CONNECTED) initWiFi();
-
-  delay(1000);
 }
