3623e29bcd40354b835dee12050501bea4ffabad
BirnadinErick <45619033+BirnadinErick@users.noreply.github.com>
2025-04-26 12:52:10 +0200

    just connect / hard code creds

--- esp32-wifi-demo/src/main.cpp
+++ esp32-wifi-demo/src/main.cpp
@@ -1,18 +1,58 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char *SERVER = "http://";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void initWiFi()
+{
+  const char *WIFI_SSID = "";
+  const char *WIFI_PASSWORD = "";
+
+  WiFi.mode(WIFI_OFF);
+  delay(1000);
+  WiFi.mode(WIFI_STA);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting to WiFi.");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    switch (WiFi.status())
+    {
+    case WL_CONNECT_FAILED:
+      Serial.println("connection failed");
+      break;
+    
+    case WL_NO_SSID_AVAIL:
+      Serial.println("ssid search failed");
+      break;
+    case WL_IDLE_STATUS:
+      Serial.println("idle status");
+      break;
+    case WL_SCAN_COMPLETED:
+      Serial.println("scan complete, connecting...");
+      break;
+    default:
+      break;
+    }
+  }
+
+  Serial.print("\nIP: ");
+  Serial.print(WiFi.localIP());
+  Serial.print('\n');
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  Serial.begin(115200);
+  initWiFi();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  if(WiFi.status() != WL_CONNECTED) initWiFi();
+
+  delay(1000);
 }
