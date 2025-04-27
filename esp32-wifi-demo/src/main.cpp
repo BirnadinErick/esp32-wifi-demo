17f0e15171eb8fc75fcb99e3cdda00d9bf0e6626
BirnadinErick <45619033+BirnadinErick@users.noreply.github.com>
2025-04-27 15:35:33 +0200

    can POST data

--- esp32-wifi-demo/src/main.cpp
+++ esp32-wifi-demo/src/main.cpp
@@ -1,11 +1,28 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiManager.h>
+#include <HTTPClient.h>
 
-const int TOUCH_SENSOR = 35;
+const int TOUCH_SENSOR = 35; // GPIO 35
 const char *INIT_AP_NAME = "beAP";
 
+// plz use https in prod, http is just for learning purposes
+const char *API = "http://192.168.201.71:2003/save-data";
+
 int sensor_value = 0;
+
+void send_data(int data)
+{
+  WiFiClient wifi_client;
+  HTTPClient http_client;
+
+  http_client.begin(wifi_client, API);
+  http_client.addHeader("Content-Type", "text/plain");
+  auto req = String(data);
+
+  int res = http_client.POST(req);
+  Serial.printf("httP res: %d\n", res);
+}
 
 void setup()
 {
@@ -17,7 +34,6 @@ void setup()
   bool res;
 
   Serial.print("connecting to WiFi...");
-  // wm.resetSettings();
   res = wm.autoConnect(INIT_AP_NAME);
 
   if (res)
@@ -36,7 +52,7 @@ void setup()
 void loop()
 {
   sensor_value = analogRead(TOUCH_SENSOR);
-  Serial.println(sensor_value);
+  send_data(sensor_value);
 
-  delay(1000);
+  delay(500);
 }
