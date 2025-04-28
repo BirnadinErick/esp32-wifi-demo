13c70ab9d81e88d20c1778157036215e207f922f
BirnadinErick <45619033+BirnadinErick@users.noreply.github.com>
2025-04-28 14:32:31 +0200

    can read a sensor

--- esp32-wifi-demo/src/main.cpp
+++ esp32-wifi-demo/src/main.cpp
@@ -5,11 +5,13 @@
 
 const int TOUCH_SENSOR = 35; // GPIO 35
 const char *INIT_AP_NAME = "beAP";
+const int TIME_INTERVAL = 5 * 1000;
 
 // plz use https in prod, http is just for learning purposes
 const char *API = "http://192.168.201.71:2003/save-data";
 
 int sensor_value = 0;
+unsigned long timestamp = 0;
 
 void send_data(int data)
 {
@@ -47,12 +49,24 @@ void setup()
   }
 
   pinMode(TOUCH_SENSOR, INPUT);
+  timestamp = millis();
 }
 
 void loop()
 {
+  if (millis() - timestamp < TIME_INTERVAL)
+  {
+    goto LOOP_TAIL;
+  }
+
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    ESP.restart();
+  }
+
   sensor_value = analogRead(TOUCH_SENSOR);
   send_data(sensor_value);
 
-  delay(500);
+LOOP_TAIL:
+  timestamp = millis();
 }
